e1647ca038abffd26da3c5f58a3fc377b30c5bce
AvijitDasAvi <avijitavi338895@gmail.com>
2025-09-03 17:15:14 +0600

    Splash Screen done.

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,12 +1,16 @@
 import 'package:elad_giserman/features/auth/sign_in/screens/sign_in_screen.dart';
+import 'package:elad_giserman/features/splash/screens/splash_screen.dart';
 import 'package:get/get.dart';
 
 class AppRoute {
+  static String splashScreen = "/splashScreen";
   static String signInScreen = "/signInScreen";
 
+  static String getSplashScreen() => splashScreen;
   static String getSignInScreen() => signInScreen;
 
   static List<GetPage> routes = [
+    GetPage(name: splashScreen, page: () => const SplashScreen()),
     GetPage(name: signInScreen, page: () => const SignInScreen()),
   ];
 }
