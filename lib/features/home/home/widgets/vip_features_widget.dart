7cc675f32896bcbb0c56bc0d5e5dc17f6e972e38
Shakil7869 <shakil35-2809@diu.edu.bd>
2026-01-08 03:13:35 +0600

    added dynamic banner in home screen

--- lib/features/home/home/widgets/vip_features_widget.dart
+++ lib/features/home/home/widgets/vip_features_widget.dart
@@ -1,20 +1,36 @@
 import 'package:elad_giserman/core/common/styles/global_text_style.dart';
 import 'package:elad_giserman/core/common/widgets/custom_small_button.dart';
-import 'package:elad_giserman/core/utils/constants/colors.dart';
 import 'package:elad_giserman/core/utils/constants/image_path.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class VipFeaturesWidget extends StatelessWidget {
-  const VipFeaturesWidget({super.key});
+  final String? bannerCard;
+  final String? bannerPhoto;
+  final String? title;
+  final String? description;
+
+  const VipFeaturesWidget({
+    super.key,
+    this.bannerCard,
+    this.bannerPhoto,
+    this.title,
+    this.description,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final displayTitle = title ?? 'vip_title'.tr;
+    final displayDescription = description ?? 'vip_description'.tr;
+    final displayBannerCard =
+        bannerCard ?? 'https://via.placeholder.com/400x200';
+    final displayBannerPhoto = bannerPhoto ?? ImagePath.strawGlass;
+
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
       decoration: BoxDecoration(
-        color: AppColors.buttonColor,
         borderRadius: BorderRadius.circular(16),
+        image: _buildBackgroundImage(displayBannerCard),
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -25,7 +41,7 @@ class VipFeaturesWidget extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'vip_title'.tr,
+                  displayTitle,
                   style: getTextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w600,
@@ -34,7 +50,7 @@ class VipFeaturesWidget extends StatelessWidget {
                 ),
                 SizedBox(height: 6),
                 Text(
-                  'vip_description'.tr,
+                  displayDescription,
                   style: getTextStyle(
                     fontSize: 12,
                     fontWeight: FontWeight.w400,
@@ -57,10 +73,42 @@ class VipFeaturesWidget extends StatelessWidget {
               ],
             ),
           ),
-
-          Image.asset(ImagePath.strawGlass, scale: 4),
+          _buildBannerPhoto(displayBannerPhoto),
         ],
       ),
     );
+  }
+
+  DecorationImage? _buildBackgroundImage(String imagePath) {
+    if (imagePath.startsWith('http')) {
+      return DecorationImage(
+        image: NetworkImage(imagePath),
+        fit: BoxFit.cover,
+      );
+    }
+    return null;
+  }
+
+  Widget _buildBannerPhoto(String imagePath) {
+    // Check if it's a URL (starts with http) or a local asset
+    if (imagePath.startsWith('http')) {
+      return Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Image.network(
+            imagePath,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Image.asset(ImagePath.strawGlass, scale: 4);
+            },
+          ),
+        ),
+      );
+    } else {
+      return Image.asset(imagePath, scale: 4);
+    }
   }
 }
